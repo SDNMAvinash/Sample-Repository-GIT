5c4ee1be716df0131e9ab1939404bcad92d0d6d5
SDNMAvinash <sdnmavinash@gmail.com>
2025-05-09 11:49:26 +0000

    committing java program

---
+++ Hello.java
@@ -0,0 +1,8 @@
+Public class Hello
+{
+	Public static void main(string [] args)
+		{
+			//prints "Hello world...This is chitti, speed 1THz & Memory 1ZB" in terminal window.
+			System.out.println("Hello world...This is chitti, speed 1THz & Memory 1ZB");
+		}
+}
